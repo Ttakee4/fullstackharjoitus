6ed2b4139aabe1523e6847113b509836eebed4a8
eemikatt <eemikatt@jyu.fi>
2024-12-12 20:28:30 +0200

    .

--- part4/mongo.js
+++ part4/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-if (process.argv.length<3) {
+if (process.argv.length < 3) {
   console.log('give password as argument')
   process.exit(1)
 }
@@ -8,33 +8,32 @@ if (process.argv.length<3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://eemikatt:${password}@cluster0.wvjmt.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
-mongoose.connect(url)
-
-const noteSchema = new mongoose.Schema({
-	content: String,
-	important: Boolean,
-})
-  
-const Note = mongoose.model('Note', noteSchema)
-
-const note = new Note({
-  content: 'Mongoose makes things easy',
-  important: true,
-})
-
-// note.save().then(result => {
-//   console.log('note saved!')
-//   mongoose.connection.close()
-// })
-
-//Note.find({ important: true }).then(result => 
-
-Note.find({}).then(result => {
-	result.forEach(note => {
-	  console.log(note)
-	})
-	mongoose.connection.close()
+mongoose.connect(url).then(() => {
+  const noteSchema = new mongoose.Schema({
+    content: String,
+    important: Boolean,
   })
+  
+  const Note = mongoose.model('Note', noteSchema)
+  
+  const note = new Note({
+    content: 'HTML is x',
+    important: true,
+  })
+  
+  /*
+  note.save().then(result => {
+    console.log('note saved!')
+    mongoose.connection.close()
+  })
+  */
+  Note.find({}).then(result => {
+    result.forEach(note => {
+      console.log(note)
+    })
+    mongoose.connection.close()
+  })
+})
