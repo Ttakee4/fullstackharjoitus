d6a25d8ac4f6941cd2c3c1fc17abe89d098bdf42
eemikatt <eemikatt@jyu.fi>
2024-12-16 21:17:28 +0200

    harjoitukset git

--- part4/mongo.js
+++ part4/mongo.js
@@ -8,7 +8,7 @@ if (process.argv.length < 3) {
 const password = process.argv[2]
 
 const url =
-  `mongodb+srv://fullstack:${password}@cluster0.o1opl.mongodb.net/noteApp?retryWrites=true&w=majority`
+  `mongodb+srv://eemikatt:${password}@cluster0.wvjmt.mongodb.net/testNoteApp?retryWrites=true&w=majority`
 
 mongoose.set('strictQuery', false)
 mongoose.connect(url).then(() => {
@@ -19,17 +19,17 @@ mongoose.connect(url).then(() => {
   
   const Note = mongoose.model('Note', noteSchema)
   
-  const note = new Note({
-    content: 'HTML is x',
-    important: true,
-  })
+  // const note = new Note({
+  //   content: 'Html is x',
+  //   important: false,
+  // })
   
-  /*
-  note.save().then(result => {
-    console.log('note saved!')
-    mongoose.connection.close()
-  })
-  */
+  
+  // note.save().then(result => {
+  //   console.log('note saved!')
+  //   mongoose.connection.close()
+  // })
+  
   Note.find({}).then(result => {
     result.forEach(note => {
       console.log(note)
